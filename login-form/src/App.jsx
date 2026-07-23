12411e97de286643c87ecf64edd603eb37ca9bfb
Noftyfootie <owolabinofisat7@gmail.com>
2026-07-23 19:37:53 +0100

    LESSON%

---
+++ login-form/src/App.jsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "./components/LoginForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h2>Hello, welcome to my website</h2>
+      <LoginForm />
+    </div>
+  );
+}
+export default App;
---
+++ login-form/src/components/LoginForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./LoginForm.css";
+export function LoginForm() {
+  const [showPassword, setShowPassword] = useState(true);
+
+  function switchPassword() {
+    if (showPassword) {
+      setShowPassword(false);
+    } else {
+      setShowPassword(true);
+    }
+  }
+  return (
+    <div>
+      <div>
+        <input placeholder="Email" type="text" className="input-text" />
+        <div className="input-password">
+          <input
+            placeholder="password"
+            className="input-text"
+            type={showPassword ? "password" : "text"}
+          />
+          <button className="show-password" onClick={switchPassword}>
+            {showPassword ? "show" : "Hide"}
+          </button>
+        </div>
+      </div>
+
+      <div className="buttonIn">
+        <button className="buttonReg">Login</button>
+        <button className="buttonReg">Sign up</button>
+      </div>
+    </div>
+  );
+}
